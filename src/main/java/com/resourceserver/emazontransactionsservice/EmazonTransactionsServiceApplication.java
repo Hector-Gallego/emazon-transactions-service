9228c434e5fc2c58dcd070b90cf651711cb51cd5
Hector Gallego <gallegohector96@gmail.com>
2024-09-17 08:27:18 -0500

    transactional microservice initial configuration

---
+++ src/main/java/com/resourceserver/emazontransactionsservice/EmazonTransactionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.resourceserver.emazontransactionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonTransactionsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmazonTransactionsServiceApplication.class, args);
+    }
+
+}
